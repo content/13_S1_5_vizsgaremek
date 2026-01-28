e79a04640659981621e1a62c723e2720af50f9cc
shiya <147606176+adiccsek@users.noreply.github.com>
2026-01-28 10:13:22 +0100

    feat: Add PostDetailsCard and Field UI components, remove courseFuntions
    Introduced PostDetailsCard for displaying post details and attachments, and added a reusable Field UI component for form layouts. Also removed the unused courseFuntions utility. Minor CSS update for custom scrollbar styling.

---
+++ web/apps/next/components/elements/posts/post-details-card.tsx
@@ -0,0 +1,38 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Post, Course, Attachment } from '@studify/types';
+import { PostCardHeader } from './post-card-header';
+import AttachmentCard from '../attachments/attachment-card';
+
+type PostDetailsCardProps = {
+    course: Course;
+    post: Post;
+}
+
+export default function PostDetailsCard({ course, post }: PostDetailsCardProps) {
+
+    return (
+        <div className="flex flex-col gap-4">
+            <Card>
+                <PostCardHeader course={course} post={post} />
+                <CardContent className="px-6">
+                    <div>
+                        <p>{post.description}</p>
+                    </div>
+                    {post.attachments.length > 0 && (
+                    <div className='mt-7'>
+                        <div>
+                            <h1 className='text-md font-bold'>Csatolmányok</h1>
+                            <p className='text-sm text-muted-foreground mb-4'>A poszthoz csatolt fájlok listája</p>
+                        </div>
+                        <div className='w-full flex flex-wrap gap-3 pe-5'>
+                            {post.attachments.map((attachment: Attachment) => ( 
+                                <AttachmentCard key={attachment.id} attachment={attachment}></AttachmentCard>
+                            ))}
+                        </div>
+                    </div>
+                    )}
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
