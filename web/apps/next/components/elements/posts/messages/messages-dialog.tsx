0117b76d0d3cf039dd7197f26235c41017ef90bc
Tigi <61912726+content@users.noreply.github.com>
2026-03-03 13:47:24 +0100

    faeat: Add message fetching and send logic to MessagesDialog

--- web/apps/next/components/elements/posts/messages/messages-dialog.tsx
+++ web/apps/next/components/elements/posts/messages/messages-dialog.tsx
@@ -1,6 +1,9 @@
+import { useNotificationProvider } from "@/components/notification-provider";
 import { DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Dialog } from "@radix-ui/react-dialog";
-import { Post, User } from "@studify/types"
+import { Course, Message, Post, User } from "@studify/types"
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 type PostMessageModalProps = {
     post: Post;
@@ -13,7 +16,76 @@ type PostMessageModalProps = {
 }
 
 export default function PostMessagesDialog({ post, sender, receiver, children, isOpen, onClose }: PostMessageModalProps) {
-    
+    const { notify } = useNotificationProvider();
+    const { data: session, status } = useSession();
+
+    const [course, setCourse] = useState<Course | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const [messages, setMessages] = useState<Message[]>([]);
+
+    const handleSendMessage = (message: string) => {
+        if(!course) return;
+
+        const response = fetch(`/api/courses/${course.id}/posts/${post.id}/messages`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                senderId: sender.id,
+                receiverId: receiver.id,
+                message
+            })
+        });
+
+        
+    }
+
+    useEffect(() => {
+        if(!session || !session.user) return;
+
+        const course = session.user.courses.find((course: Course) => course.posts.some((p: Post) => p.id === post.id));
+        if(!course) return;
+
+        setCourse(course);
+
+        const fetchMessages = async () => {
+            const response = await fetch(`/api/courses/${course.id}/posts/${post.id}/messages`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    senderId: sender.id,
+                    receiverId: receiver.id
+                })
+            });
+        
+            if(!response.ok) {
+                notify("Hiba történt az üzenetek betöltésekor", { type: "error" });
+            }
+
+            switch(response.status) {
+                case 200:
+                    const data = await response.json();
+                    setMessages(data.messages);
+                    setIsLoading(false);
+                    break;
+
+                case 404:
+                    notify("Nem található a kurzus vagy a poszt", { type: "error" });
+                    setIsLoading(false);
+                    break;
+                    
+                default:
+                    notify("Hiba történt az üzenetek betöltésekor", { type: "error" });
+                    setIsLoading(false);
+            }
+        };
+
+        fetchMessages();
+    }, [session, post, sender, receiver])
     
     return (
         <Dialog open={isOpen} onOpenChange={(open) => {
